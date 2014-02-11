945874bcaee1f653d6684153fea6acef4200cfe2
THOMAS POCREAU <thomas.poc@gmail.com>
2014-02-11 14:13:44 +0100

    change answer to question 1 and 2

--- train.m
+++ train.m
@@ -99,7 +99,7 @@ for epoch = 1:epochs
 
     %% HIDDEN LAYER.
     % FILL IN CODE. Replace the line below by one of the options.
-    embed_to_hid_weights_gradient = embedding_layer_state * back_propagated_deriv_1';
+    embed_to_hid_weights_gradient = embedding_layer_state;
     % Options:
 	%     numhid1*numwords X numhid2      numhid1*numwords X batchsize  * numhid2 X batchsize 
     % (a) embed_to_hid_weights_gradient = back_propagated_deriv_1' * embedding_layer_state;
@@ -108,7 +108,7 @@ for epoch = 1:epochs
     % (d) embed_to_hid_weights_gradient = embedding_layer_state;
 	
     % FILL IN CODE. Replace the line below by one of the options.
-    hid_bias_gradient = sum(back_propagated_deriv_1, 2);
+    hid_bias_gradient = back_propagated_deriv_1';
     % Options
     % (a) hid_bias_gradient = sum(back_propagated_deriv_1, 2);
     % (b) hid_bias_gradient = sum(back_propagated_deriv_1, 1);
