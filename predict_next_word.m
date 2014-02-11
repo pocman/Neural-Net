c942e1a8fe10c44bb90b96a75cd97a24624f137b
THOMAS POCREAU <thomas.poc@gmail.com>
2014-02-11 15:03:37 +0100

    suppress

--- predict_next_word.m
+++ predict_next_word.m
@@ -5,20 +5,9 @@ function predict_next_word(word1, word2, word3, model, k)
 %   word2: The second word as a string.
 %   word3: The third word as a string.
 %   model: Model returned by the training script.
-%   k: The k most probable predictions are shown.
+%   k: The k most prhefkjezkjfobable predictions are shown.
 % Example usage:
-%   predict_next_word('john', 'might', 'be', model, 3);
-%   predict_next_word('life', 'in', 'new', model, 3);
-
-word_embedding_weights = model.word_embedding_weights;
-vocab = model.vocab;
-id1 = strmatch(word1, vocab, 'exact');
-id2 = strmatch(word2, vocab, 'exact');
-id3 = strmatch(word3, vocab, 'exact');
-if ~any(id1)
-  fprintf(1, 'Word ''%s\'' not in vocabulary.\n', word1);
-  return;
-end
+%   pred
 if ~any(id2)
   fprintf(1, 'Word ''%s\'' not in vocabulary.\n', word2);
   return;
